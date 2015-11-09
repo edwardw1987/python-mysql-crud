adc4d2b4452b5bd27753bf64791f35cdc6998afb
edward <edwardw@qq.com>
2015-11-09 20:57:33 +0800

    ok

--- __init__.py
+++ __init__.py
@@ -3,5 +3,6 @@
 # @Author: edward
 # @Date:   2015-11-06 11:21:15
 # @Last Modified by:   edward
-# @Last Modified time: 2015-11-07 23:34:43
+# @Last Modified time: 2015-11-09 20:56:53
 from .database import DataBase
+from .crud import *
