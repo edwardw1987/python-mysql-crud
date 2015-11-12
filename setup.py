0563fee48df3b11d10345414280d7ff8f19e1bc5
edward <edwardw@qq.com>
2015-11-12 18:23:54 +0800

    ok

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 	name='mycrud',
 	version='0.1.1',
 	description='python-mysql-crud',
-	long_description=open('README.md').read(),
+	long_description=open('README.rst').read(),
 	author='Edward Wang',
 	author_email='edwardw@qq.com',
 	url='https://github.com/edwardw1987/python-mysql-crud',
