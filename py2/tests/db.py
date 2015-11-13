2b228baba30b2d0f7f22bd1d1ee45a763cd2bbce
edward <edwardw@qq.com>
2015-11-13 11:30:10 +0800

    ok

--- py2/tests/db.py
+++ py2/tests/db.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2015-11-06 11:54:09
 # @Last Modified by:   edward
-# @Last Modified time: 2015-11-11 17:27:24
+# @Last Modified time: 2015-11-11 17:21:24
 import sys
 sys.path.append('../')
 from mycrud import DataBase
