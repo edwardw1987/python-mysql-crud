afec7a70f0089c2bc5ed37c8c86bcad2a22ec4d9
edward <edwardw@qq.com>
2015-11-13 11:19:51 +0800

    ok

--- py2/mycrud/utils.py
+++ py2/mycrud/utils.py
@@ -3,7 +3,7 @@
 # @Author: edward
 # @Date:   2015-11-06 11:29:13
 # @Last Modified by:   edward
-# @Last Modified time: 2015-11-12 17:13:13
+# @Last Modified time: 2015-11-13 11:16:49
 from MySQLdb.cursors import DictCursor 
 from MySQLdb.connections import Connection 
 
@@ -48,7 +48,7 @@ class Storage(dict):
     def __getattr__(self, key):
         try:
             return self[key]
-        except KeyError(k):
+        except KeyError as k:
             raise AttributeError(k)
 
     def __setattr__(self, key, value):
@@ -57,7 +57,7 @@ class Storage(dict):
     def __delattr__(self, key):
         try:
             del self[key]
-        except KeyError(k):
+        except KeyError as k:
             raise AttributeError(k)
 
     def __repr__(self):
